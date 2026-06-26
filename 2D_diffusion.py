f7cd8e8047d91db265e296873d7a643303b75957
andreagrimaldi <ag@andreagrimaldi.eu>
2026-06-26 20:25:32 -0300

    First commit

---
+++ 2D_diffusion.py
@@ -0,0 +1,12 @@
+'''
+CFD @ FIE
+A.Y. 2025-2026
+Professor: Edgardo A. Serafin
+Student: Andrea Grimaldi
+
+Final exam
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
---
+++ README.md
@@ -0,0 +1,6 @@
+# CFD Final Exam 
+## 1D Unsteady convection (Burgers equation)
+
+FIE-UNDEF · Introducción a la Dinámica de Fluidos Computacional · 2026-S1
+
+---
